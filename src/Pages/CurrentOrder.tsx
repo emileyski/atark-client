ba97bbb90afa23138e476758a13df6e359fbc3b7
emileyski <96009930+emileyski@users.noreply.github.com>
2023-11-06 12:49:41 +0200

    fix: обновил current order

--- src/Pages/CurrentOrder.tsx
+++ src/Pages/CurrentOrder.tsx
@@ -4,7 +4,7 @@ import getAxiosInstance from "src/api/interceptors";
 import { useLoaderData } from "react-router-dom";
 import OrderItem from "@Features/Order/Components/OrderItem";
 import { MapContainer, TileLayer } from "react-leaflet";
-import { Container } from "@mui/material";
+import { Container, Box } from "@mui/material";
 
 import L from "leaflet";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
@@ -60,34 +60,36 @@ const CurrentOrderRouting: React.FC<{
     }).addTo(map);
 
     return () => map.removeControl(routingControl);
-  }, [map, originPosition, destinationPosition]);
+  }, [map, originPosition, destinationPosition, originName, destinationName]);
 
   return null;
 };
-
 const CurrentOrder: React.FC = () => {
   const currentOrder: IOrder = useLoaderData() as IOrder;
 
   return (
-    <Container sx={{ pt: 2 }}>
-      <OrderItem order={currentOrder} />
-      <MapContainer
-        center={[0, 0]}
-        zoom={5}
-        style={{ height: "500px", margin: "1rem" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
+    <Container maxWidth="xl" sx={{ pt: 2, display: "flex" }}>
+      {/* OrderItem слева */}
+      <Box flex="1" pr={2}>
+        <OrderItem order={currentOrder} />
+      </Box>
 
-        <CurrentOrderRouting
-          originName={currentOrder.origin}
-          destinationName={currentOrder.destination}
-          destinationPosition={currentOrder.destinationCoordinates}
-          originPosition={currentOrder.originCoordinates}
-        />
-      </MapContainer>
+      {/* Карта справа */}
+      <Box flex="1" p={1}>
+        <MapContainer center={[0, 0]} zoom={5} style={{ height: "100%" }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+
+          <CurrentOrderRouting
+            originName={currentOrder.origin}
+            destinationName={currentOrder.destination}
+            destinationPosition={currentOrder.destinationCoordinates}
+            originPosition={currentOrder.originCoordinates}
+          />
+        </MapContainer>
+      </Box>
     </Container>
   );
 };
